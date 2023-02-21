5cf0084940edc3561d837e8f906945cef81dc6eb
zalmon74 <zalmon74@yandex.ru>
2023-02-21 23:16:37 +0700

    Сформирован запуск программы формирования битовой карты

--- bitmap_3/main.py
+++ bitmap_3/main.py
@@ -1,3 +1,13 @@
 """ Основной файл проекта для запуска
 """
 
+from bitmap import BitMap
+
+
+def main():
+    programm = BitMap()
+    programm.start()
+    
+
+if __name__ == '__main__':
+    main()
