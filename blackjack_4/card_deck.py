d7373572101f96b320b46adb3e24ac170f27027a
zalmon74 <zalmon74@yandex.ru>
2023-02-22 15:50:24 +0700

    Добавлена функция генерации строки для красивого отображения карт в консоли

--- blackjack_4/card_deck.py
+++ blackjack_4/card_deck.py
@@ -3,6 +3,39 @@
 
 from dataclasses import dataclass
 
+from constants import Constants
+
+
+def list_card_2_str_for_print(lst_cards: list, sep: str = '\t') -> str:
+    """ Функция генерирует строку, которая отображает красивую форму карт в 
+        консоли
+
+    Args:
+        lst_cards (list): Список с картами, которые необходимо превратить в строку
+
+    Returns:
+        str: Строка, которая красиво отображает карты в консоле
+    """
+    output = ''
+    for num_str in range(4):
+        for card in lst_cards:
+            if num_str == 0:
+                output += ' ___'
+            if num_str == 1:
+                if len(card.name) == 2:
+                    output += f'|{card.name} |'
+                else:
+                    output += f'|{card.name}  |'
+            if num_str == 2:
+                output += f'| {card.suit} |'
+            if num_str == 3:
+                if len(card.name) == 2:
+                    output += f'|_{card.name}|'
+                else:
+                    output += f'|__{card.name}|'
+            output += sep
+        output += '\n'
+    return output
 
 @dataclass
 class Card:
@@ -17,6 +50,5 @@ class Card:
     alter_points: int  # Альтернативное количество очков, которое приносит карта (необходимо для туза)
     
     def __str__(self) -> str:
-        return f' ___\n|{self.name}  |\n| {self.suit} |\n|__{self.name}|\n'
+        return list_card_2_str_for_print([self,])
     
-
