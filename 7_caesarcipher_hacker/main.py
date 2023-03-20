83de9e0d05d8ff9808cf649a3e988b3002a55d83
zalmon74 <zalmon74@yandex.ru>
2023-03-20 17:55:17 +0700

    Исправлен файл запуска

--- 7_caesarcipher_hacker/main.py
+++ 7_caesarcipher_hacker/main.py
@@ -1,2 +1,13 @@
 """ Основной файл запуска программы
 """
+
+from cli.cli import CLICaesarCipherHack
+
+
+def main():
+    program = CLICaesarCipherHack()
+    program.start()
+    
+    
+if __name__ == '__main__':
+    main()
